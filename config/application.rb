3bb87cd9521be0665d11668b865c45356284c855
Colin Ross <cross@parallel6.com>
2016-08-31 10:42:15 -0700

    Logging to STDOUT w/tagging

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module HipsterHair
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.log_level = :debug
+    config.log_tags  = [:uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
   end
 end
